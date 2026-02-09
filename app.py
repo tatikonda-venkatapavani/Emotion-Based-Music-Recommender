b92a2401a340150ce29f64bdd1515462164bbf9c
Pavani Tatikonda <venkatapavani1403@gmail.com>
2026-02-09 21:19:33 +0530

    Refactor app for mood-based music recommendations

--- app.py
+++ app.py
@@ -1,70 +1,62 @@
 import streamlit as st
 import random
 
-# 1. App Configuration
-st.set_page_config(page_title="Global MoodTune", page_icon="🎵")
+st.set_page_config(page_title="VibeCheck Music", page_icon="🎵")
 
-st.markdown("""
-    <style>
-    .main { background-color: #121212; color: white; }
-    .stButton>button { width: 100%; border-radius: 20px; background-color: #1DB954; color: white; }
-    </style>
-    """, unsafe_allow_html=True)
-
-# 2. Expanded Multi-Language Database
+# --- DATABASE LOGIC ---
+# Strictly categorized by actual musical vibe
 music_db = {
-    "English": {
-        "Happy": ["Walking on Sunshine - Katrina & The Waves", "Happy - Pharrell Williams"],
-        "Sad": ["Someone Like You - Adele", "Fix You - Coldplay"],
-        "Energetic": ["Power - Kanye West", "Eye of the Tiger - Survivor"],
-        "Relaxed": ["Sunflower - Post Malone", "Weightless - Marconi Union"]
-    },
     "Telugu": {
-        "Happy": ["Butta Bomma (AVPL)", "Chilipiga (Orange)", "Super Machi (S/O Satyamurthy)"],
-        "Sad": ["Oosupodu (Fidaa)", "Thalachi Thalachi (7G Brindavan Colony)"],
-        "Energetic": ["Top Lesi Poddi (Iddarammayilatho)", "Ramulo Ramula (AVPL)"],
-        "Relaxed": ["Samayama (Hi Nanna)", "Gundelonaa (Ori Devuda)"]
+        "Happy": ["Butta Bomma (AVPL)", "Chilipiga (Orange)", "Super Machi (S/O Satyamurthy)", "Ramulo Ramula", "Inkem Inkem Kaavaale", "Cinema Choopistha Mava", "Blockbuster", "He's So Cute", "Dhimmathirigae", "Aagadu Title"],
+        "Sad": ["Oosupodu (Fidaa)", "Thalachi Thalachi (7G)", "Evare (Premam)", "Urike Chilaka", "Adiga Adiga (Ninnu Kori)", "Priyathama (Majili)", "Manasa (Munna)", "Life of Ram", "Yemainado", "Kanulanu Chade"],
+        "Energetic": ["Top Lesi Poddi", "Dhamki Title", "Kurchi Madathapetti", "Bullet Song", "Naatu Naatu", "Jai Balayya", "Srivalli", "Mind Block", "Jigelu Rani", "Saami Saami"],
+        "Romantic": ["Samajavaragamana", "Maate Vinadhuga", "Ay Pilla", "Emito Idhi", "Kannullona", "Neevalle", "Hridayam Lopala", "Nadhive", "Mellaga Karagani", "Undiporaadhey"],
+        "Angry": ["Dabidi Dibidi", "Ragile Ragile", "Thee Thalapathy (Telugu)", "Shadow Title", "Don Seenu", "Mirchi Title", "Baahubali (Shivuni Aana)", "Bhairava Anthem", "Singham Title", "Bheemla Nayak Title"]
     },
     "Hindi": {
-        "Happy": ["Balam Pichkari (YJHD)", "London Thumakda (Queen)"],
-        "Sad": ["Agar Tum Saath Ho (Tamasha)", "Channa Mereya (ADHM)"],
-        "Energetic": ["Mauja Hi Mauja (Jab We Met)", "Kar Gayi Chull"],
-        "Relaxed": ["Tum Se Hi (Jab We Met)", "Iktara (Wake Up Sid)"]
+        "Happy": ["Balam Pichkari", "London Thumakda", "Mauja Hi Mauja", "Galla Goodiyan", "Zingaat", "Badtameez Dil", "Aankh Marey", "Kala Chashma", "Abhi Toh Party", "Kar Gayi Chull"],
+        "Sad": ["Agar Tum Saath Ho", "Channa Mereya", "Tum Hi Ho", "Kabira", "Tujhe Kitna Chahne Lage", "Bekhayali", "Humari Adhuri Kahani", "Duaa", "Abhi Mujh Mein Kahin", "Phir Le Aya Dil"],
+        "Energetic": ["Malhari", "Zinda (BMB)", "Sultan Title", "Dhating Naach", "Sheila Ki Jawani", "Dilbar", "Bom Diggy Diggy", "Garmi", "Jai Jai Shivshankar", "Muqabla"],
+        "Romantic": ["Raataan Lambiyan", "Pee Loon", "Tum Kya Mile", "Pal Ek Pal", "Hawayein", "Ban Ja Rani", "Qaafirana", "Nazm Nazm", "Dil Diyan Gallan", "Kesariya"],
+        "Angry": ["Aarambh Hai Prachand", "Sadda Haq", "Apna Time Aayega", "Bhaag D.K. Bose", "Gully Boy Rap", "Khalibali", "Agneepath Theme", "Mera Wala Dance", "Zinda", "Sultan Title"]
     },
-    "Tamil": {
-        "Happy": ["Arabic Kuthu (Beast)", "Aaluma Doluma (Vedalam)"],
-        "Sad": ["Poo Nee Poo (3)", "Kanave Kanave (David)"],
-        "Energetic": ["Vaathi Coming (Master)", "Rakita Rakita (Jagame Thandhiram)"],
-        "Relaxed": ["Munbe Vaa (Sillunu Oru Kaadhal)", "Vaseegara (Minnale)"]
+    "English": {
+        "Happy": ["Happy - Pharrell", "Uptown Funk", "Shake It Off", "Can't Stop The Feeling", "Levitating", "Dynamite", "Stay", "I'm Yours", "Sugar", "Shut Up and Dance"],
+        "Sad": ["Someone Like You", "Fix You", "Stay With Me", "The Night We Met", "Hurt", "Driver's License", "Let Her Go", "Lovely", "Falling", "Yesterday"],
+        "Energetic": ["Power - Kanye", "Eye of the Tiger", "Thunderstruck", "Believer", "Can't Hold Us", "Lose Yourself", "Blinding Lights", "Stronger", "Till I Collapse", "Centuries"],
+        "Romantic": ["Perfect", "All of Me", "Thinking Out Loud", "Just The Way You Are", "A Thousand Years", "Say You Won't Let Go", "Lover", "My Heart Will Go On", "Beautiful In White", "Shallow"],
+        "Angry": ["In The End", "Killing In The Name", "Smells Like Teen Spirit", "Break Stuff", "Walk (Pantera)", "Down With The Sickness", "One Step Closer", "Bulls On Parade", "Remember The Name", "Numb"]
     }
 }
 
-# 3. App UI
-st.title("🎵 Global MoodTune")
-st.subheader("Select your language and mood for a recommendation.")
+# --- UI INTERFACE ---
+st.title("🎧 Precise Mood Recommender")
+st.write("Ensuring the music actually matches your current state.")
 
-# Language Selection
-selected_lang = st.selectbox("Choose your Preferred Language:", list(music_db.keys()))
+# Selectors
+col_a, col_b = st.columns(2)
+with col_a:
+    lang = st.selectbox("Language", list(music_db.keys()))
+with col_b:
+    mood = st.selectbox("Current Mood", list(music_db[lang].keys()))
 
-st.write(f"How is your mood in {selected_lang}?")
-
-# 4. Logic & Display
-moods = ["Happy", "Sad", "Energetic", "Relaxed"]
-cols = st.columns(4)
-selected_mood = None
-
-for i, mood in enumerate(moods):
-    if cols[i].button(mood):
-        selected_mood = mood
-
-if selected_mood:
+if st.button(f"Generate {mood} Playlist"):
     st.divider()
-    # Pulling from the nested dictionary based on Lang -> Mood
-    songs = music_db[selected_lang][selected_mood]
-    recommendation = random.choice(songs)
     
-    st.markdown(f"### Current Mood: **{selected_mood}**")
-    st.success(f"**Recommended {selected_lang} Track:** \n\n {recommendation}")
-    st.info(f"Go play this on Spotify or YouTube!")
-else:
-    st.info("Pick a mood to see the magic.")
+    # Logic: Get the specific list for Lang + Mood
+    playlist = music_db[lang][mood]
+    
+    # Shuffle so it's not the same order every time
+    random.shuffle(playlist)
+    
+    st.subheader(f"Showing top {len(playlist)} tracks for {mood} ({lang})")
+    
+    # Visualization of the Logic
+    
+    
+    for i, song in enumerate(playlist, 1):
+        # Create a search query link
+        search_query = f"https://www.youtube.com/results?search_query={song.replace(' ', '+')}"
+        st.markdown(f"**{i}. {song}** &nbsp; [Listen on YouTube]({search_query})")
+    
+    st.success("Playlist synchronized to your mood!")
